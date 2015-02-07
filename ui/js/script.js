ace8426dd99c0abb9df58006f83dab620af2c4b7
Oana <gagea_oana@yahoo.com>
2015-02-08 01:07:19 +0200

    Schematic "edit profile" - done.

--- ui/js/script.js
+++ ui/js/script.js
@@ -197,19 +197,48 @@ jQuery(document).ready(function(){
 	});
 	
 	/*Popup - Edit profile-----------------------------------------------------------------*/
+	//
+	var d = new Date();
+	var month = d.getMonth()+1;
+	var day = d.getDate();
+
+	var output =  ((''+day).length<2 ? '0' : '') + day   + '/' +
+	    ((''+month).length<2 ? '0' : '') + month + '/' +
+	  	 d.getFullYear();
+
+	var age = $("#age-profile").text();
+	var age_values = age.split("/");
+	var age_user = d.getFullYear();
+
+	//if()
+	var dif = parseInt(age_user) - parseInt(age_values[2]);
+	$("#years-profile").text(dif);
+	
+ 	 
 	$("#edit-profile").click(function() {
 		$("#profile-edit").fadeIn(500);//.css("display","block");
 			var name = $('#name-profile').text();
 			console.log(name);
-			$('#new-name').val("name");
+			$('#new-name').val(name);
 
 			var age = $("#age-profile").text();
 			console.log(age);
-			$('#new-age').val(age);
+			//$('#new-age').val(age);
+			$( "#datepicker" ).datepicker({ 
+	   			minDate: '-100Y',
+	   			maxDate: "0",
+	   			changeMonth: true,
+	      		changeYear: true,
+	      		dateFormat: "dd/mm/yy",
+	      		 
+   			 });
+ 			$("#datepicker").datepicker('setDate', age);
+
 
 			var gender = $('#gender-profile').text();
 			console.log(gender);
 			$('#gender-new').val(gender);
+
 
 			if(gender === "fem"){
 				$("#new-fem").attr('checked', 'checked');
@@ -219,27 +248,127 @@ jQuery(document).ready(function(){
 
 			var religion = $("#religion-profile").text();
 			//console.log(religion);
-
+			var true_religion ="";
 			$("#new-religion").find('option').each(function()
 			{
 			    // add $(this).val() to your list
 			    if($(this).val() === religion){
-			    	//console.log($(this).val());
-			    	$(this).selected = true;
+			    	//console.log($(this).val()+"---- "+ religion);
+			    	$("#new-religion").val(religion);
+			    	
+			    	true_religion =  religion; 
 			    }
 			    	
 			});
 
 
+			$("#done-edit").click(function() {
+				var name_new_value = $("#new-name").val();
+				$("#name-profile").text(name_new_value);
 
-		/*Creez un obiect Json si in adaug in Parse*/
+				var birthday_new_value= $("#datepicker").datepicker().val();
+				$("#age-profile").text(birthday_new_value);
+
+				var religion_new_value = $("#new-religion").val();
+				$("#religion-profile").text(religion_new_value);
+
+				var gender_new_value ="";
+				if($('#new-fem').is(':checked')){
+					gender_new_value = "fem";
+
+				}else {
+					gender_new_value = "masc";
+
+				}
 
 
+				/*Creez un obiect Json si in adaug in Parse*/
+				var d = new Date();
+				var month = d.getMonth()+1;
+				var day = d.getDate();
+
+				var output =  ((''+day).length<2 ? '0' : '') + day   + '/' +
+				    ((''+month).length<2 ? '0' : '') + month + '/' +
+				  	 d.getFullYear();
+
+				var age = $("#age-profile").text();
+				var age_values = age.split("/");
+				var age_user = d.getFullYear();
+
+				//if()
+				var dif = parseInt(age_user) - parseInt(age_values[2]);
+				$("#years-profile").text(dif);
+				var jsonValue = {};
+				jsonValue = {
+						'name': name_new_value,
+						'birthday' : birthday_new_value,
+						'age':  dif,
+						'gender' : gender_new_value,
+						'religion' : $("#new-religion").val() 
+
+				};
+				//console.log(jsonValue);
+				/*Add new row in parse -------------------------------------------------------- */
+				var Users = Parse.Object.extend("Users");
+				var query = new Parse.Query(Users);
+
+				var email_login = $.cookie("email");
+				query.equalTo("email", email_login);
+					query.find({
+					  success: function(results) {
+					   // alert("Successfully retrieved " + results.length + " scores.");
+					    // Do something with the returned Parse.Object value
+					    //console.log(results);
+					    results[0].set('information', jsonValue);
+					    results[0].save();
+					   
+						 $("#profile-edit").fadeOut(500);//.css("display","none"); 
+						 $('#update_info').removeClass().addClass('alert alert-success').html('Informations have been successfully updated.');
+						 $("#update_info").fadeOut(7000);
+
+					  },
+					  error: function(error) {
+					    $('#update_info').removeClass().addClass('alert alert-error').html('Error');
+					     $("#update_info").fadeOut(7000);
+					  }
+				});
+
+
+		});
+	
 
 
 	});
 	
+	/*User information taked from parse ---------------------------------------------------------------------------------*/
+	var Users = Parse.Object.extend("Users");
+	var query = new Parse.Query(Users);
+	var email_login = $.cookie("email");
+	query.equalTo("email", email_login);
+		query.find({
+			success: function(results) {
+					  
+				result_information = results[0].get('information');
+				console.log(result_information);
+				console.log(result_information["age"]);
+					$("#name-profile").text(result_information["name"]);	
+					$("#age-profile").text(result_information["birthday"]);
 
+
+
+					$("#years-profile").text(result_information["age"]);
+					$("#religion-profile").text(result_information["religion"]);
+					$("#gender-profile").text(result_information["gender"]);
+					   
+				},
+				error: function(error) {
+						   alert("Error - update information");
+				}
+	});
+
+
+
+	/*-------------------------------------------------------------*/
 
 
 
