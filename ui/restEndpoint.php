21de126736d2168c353eac63d75c4fd3f15fc98d
danielamariei <daniel.g.amariei@gmail.com>
2015-02-11 20:37:50 -0800

    Updated the ontology.

--- ui/restEndpoint.php
+++ ui/restEndpoint.php
@@ -1,3 +1,28 @@
 <?php
-	echo 'mata'
+	$endpoints = [
+		"events" => "http://riquack-n61vn:9000/events",
+		"clothingTypes" => "http://riquack-n61vn:9000/clothingTypes",
+		"clothingSizes" => "http://riquack-n61vn:9000/clothingSizes",
+		"clothingStyles" => "http://riquack-n61vn:9000/clothingStyles",
+		"religions" => "http://riquack-n61vn:9000/religions",
+		"seasons" => "http://riquack-n61vn:9000/seasons",
+		"weatherConditions" => "http://riquack-n61vn:9000/weatherConditions",
+		"clothingMaterials" => "http://riquack-n61vn:9000/clothingMaterials",
+		"colors" => "http://riquack-n61vn:9000/colors",
+		"events" => "http://riquack-n61vn:9000/events",
+		"events" => "http://riquack-n61vn:9000/events",
+		"events" => "http://riquack-n61vn:9000/events",
+		"events" => "http://riquack-n61vn:9000/events",
+		"events" => "http://riquack-n61vn:9000/events",
+		];
+		
+		
+		$url = $endpoints[$_GET['endpoint']];
+		$method = $_GET["method"];
+			
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+		$response = curl_exec($curl);
+		
+		echo $response;
 ?>
